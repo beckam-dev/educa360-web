995c13755a8f4be3839b090041327a02c8177ad3
beckam-dev <beckamDev@hotmail.com>
2026-09-15 00:00:46 -0500

    backend --base

---
+++ performance-tracking/src/main/java/com/educa360/performance_tracking/PerformanceTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.educa360.performance_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceTrackingApplication.class, args);
+	}
+
+}
---
+++ performance-tracking/src/test/java/com/educa360/performance_tracking/PerformanceTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educa360.performance_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
